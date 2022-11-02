f834ee5f0d640238f4f726ad3a9a17ab70f8761b
Luis Eduardo Ramírez Montoya <luiseduardoramirezmontoya@ciencias.unam.mx>
2022-11-02 15:04:06 -0600

    Remove old scripts

--- scripts/laplace.jl
+++
@@ -1,9 +0,0 @@
-using TaylorOrbits, SPICE, JLD2
-
-furnsh("naif0012.tls")
-
-radecs = MPCRadec("data/99924.txt");
-
-eph = JLD2.load("sseph313ast016_p31y_et_MIZTLI.jld2", "ss16ast_eph")
-
-laplace(radecs[1:5], eph)
